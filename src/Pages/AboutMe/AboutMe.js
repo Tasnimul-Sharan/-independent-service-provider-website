5c9bc7d96edad0dcd5cb51d9c6ee150373f9f3fb
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-04-18 02:02:04 +0600

    completed about me page

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -3,10 +3,24 @@ import pic from "../../images/Sharan.jpg.jpg";
 
 const AboutMe = () => {
   return (
-    <div>
-      <h1>about me</h1>
-      <img className="img-fluid w-25" src={pic} alt="" />
-    </div>
+    <section>
+      <div>
+        <h4>Tasnimul Alam</h4>
+        <img className="img-fluid w-25" src={pic} alt="" />
+      </div>
+      <div>
+        <h5>
+          I was started a journey when i have no idea about web development.Now
+          i can make a single page application with routing and
+          Authentication.Now my goal is to become a professonal full Stack Web
+          Developer.For this i have to search the resources and make notes
+          everyday and practice it more and more, And think about the learnning
+          part that how can apply it to in a project.I want to learn everything
+          what i have have to learn , i want to do everything, what i have to
+          do.Now web development is my part of life. Thank you.{" "}
+        </h5>
+      </div>
+    </section>
   );
 };
 
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -4,34 +4,37 @@ const Blogs = () => {
   return (
     <section>
       <div>
-        <h3>
+        <h5>
           Why are you using firebase? What other options do you have to
           implement authentication?
-        </h3>
-        <h5>
+        </h5>
+        <p>
           <span>
-            i am using firebse because it is owned by google, it is a commplete
-            package of products that allows us to build web and mobile apps
-            improves the app quality and help your clients grow their business.
+            i am using firebse because it is a commplete package of products
+            that allows us to build web and mobile application . it improves the
+            application quality and help your clients grow their business. it
+            creates Application without backend server.It brings a highly secure
+            authentication system that improves the user experience while they
+            sign-up and sign-in.
           </span>
           <br />
           <span>
             The other options in authentication those are given bellow :
             1.cards, 2.retina scans, 3.voice recognition, 4.fingerprints.
           </span>
-        </h5>
+        </p>
       </div>
       <div>
-        <h3>
-          What other services does firebase provide other than authentication ?
-        </h3>
         <h5>
+          What other services does firebase provide other than authentication ?
+        </h5>
+        <p>
           Other services does firebase provide other than authentication are
           given bellow : <br />
           1. Cloud Firestore 2. Cloud Functions 3. Google Analytics 4.Hosting
-          5.Cloud Storage 6.Authentication 7.Predictions 6. Remote Config
-          7.Dynamic Links 8.Cloud Messaging .
-        </h5>
+          5.Cloud Storage 6.Predictions 7. Remote Config 8.Dynamic Links 9.Cloud
+          Messaging .
+        </p>
       </div>
     </section>
   );
