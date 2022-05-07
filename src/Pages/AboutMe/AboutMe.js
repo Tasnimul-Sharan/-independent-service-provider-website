9ed07fecd1400d0642614c2c9f87c6bb8ebc811d
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-05-08 00:32:55 +0600

    updated

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -1,24 +1,31 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import pic from "../../images/Sharan.jpg.jpg";
 
 const AboutMe = () => {
   return (
     <section>
-      <div>
-        <h4>Tasnimul Alam</h4>
-        <img className="img-fluid w-25" src={pic} alt="" />
-      </div>
-      <div>
-        <h5>
-          I was started a journey when i have no idea about web development.Now
-          i can make a single page application with routing and
-          Authentication.Now my goal is to become a professonal full Stack Web
-          Developer.For this i have to search the resources and make notes
-          everyday and practice it more and more, And think about the learnning
-          part that how can apply it to in a project.I will give plenty of time,
-          I will give my best to reach my goal.Now web development is my part of
-          life. Thank you.{" "}
-        </h5>
+      <div className="container my-5">
+        <div className="ms-3">
+          <Card className="shadow-lg" style={{ width: "20rem" }}>
+            <Card.Img variant="top" src={pic} />
+            <Card.Body>
+              <Card.Title>Tasnimul Alam</Card.Title>
+              <Card.Text>
+                I was started a journey when i have no idea about web
+                development.Now i can make a single page application with
+                routing and Authentication and make a connection between
+                client-side and server-side with database Mongodb.Now my goal is
+                to become a professonal full Stack Web Developer.For this i have
+                to search the resources and make notes everyday and practice it
+                more and more, And think about the learnning part that how can
+                apply it to in a project.I will give plenty of time, I will give
+                my best to reach my goal.Now web development is my part of life.
+                Thank you.{" "}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
     </section>
   );
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -6,10 +6,29 @@ import {
 import auth from "../../../firebase.init";
 import google from "../../../images/google.png";
 import github from "../../../images/github.png";
+import Loading from "../../Shared/Loading/Loading";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+
+  const navigate = useNavigate();
+
+  let location = useLocation();
+  const from = location.state?.from?.pathName || "/";
+
+  if (user || user1) {
+    navigate(from, { replace: true });
+  }
+
+  if (error || error1) {
+    console.error(error, error1);
+  }
+
+  if (loading || loading1) {
+    return <Loading />;
+  }
 
   return (
     <section>
